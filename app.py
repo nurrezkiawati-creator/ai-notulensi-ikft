666f2b10be276eff44519660cc2bb7b586ba5358
nurrezkiawati-creator <nurrezkiawati@gmail.com>
2026-03-01 14:38:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ with col2:
 
 st.markdown("<hr style='border:2px solid black;'>", unsafe_allow_html=True)
 
-st.title("IU Setditjen IKFT Smart Meeting Minutes System (AI-Powered Professional Notulensi & Action Tracker)")
+st.title("IKFT Smart Meeting Minutes System (AI-Powered Professional)")
 
 # =============================
 # INPUT
