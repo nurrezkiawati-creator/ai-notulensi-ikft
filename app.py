bbab060e6cc30d35696ace05b1051c6d63bb50e2
nurrezkiawati-creator <nurrezkiawati@gmail.com>
2026-03-27 22:08:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -397,7 +397,7 @@ if st.button("Export PDF"):
         # ================= OUTER BOX =================
 
          for line in isi_notulen.split("\n"):
-             ...
+             
         
         elements.append(Spacer(1, 20))
         elements.append(Paragraph("Sekretaris Direktorat Jenderal", styles["Right"]))
